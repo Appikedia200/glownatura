245e653090ceaee36f1d1f7587a8ba4e5cb396cc
Appikedia <derrick34@mail.com>
2025-09-04 01:24:10 +0100

    update

--- src/components/FlyerPair.tsx
+++ src/components/FlyerPair.tsx
@@ -8,17 +8,18 @@ export default function WholesaleBanner() {
     <section className="py-0 bg-white">
       <div className="w-full">
           
-        {/* Full-Width Wholesale Banner */}
+        {/* Full-Width Wholesale Banner - Mobile Optimized */}
               <Link
         href="/maintenance" 
           className="block group cursor-pointer"
           >
-          <div className="relative w-full h-[300px] lg:h-[400px]">
+          <div className="relative w-full min-h-[300px] h-auto">
             <Image
               src="/images/banners/Wholesales.png"
               alt="Wholesale Program - Join Our Network"
-              fill
-              className="object-cover"
+              width={1200}
+              height={600}
+              className="w-full h-auto object-contain"
               sizes="100vw"
               priority
             />
